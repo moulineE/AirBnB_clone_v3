90796c9fc2b40663f9c91b07c95a38abf9d17f14
moulineE <elmahdi.mouline@live.fr>
2023-10-30 05:06:07 +0100

    Task 3. Status of your API FIX: testing for req4

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,9 @@
 this module is the flask app
 """
 
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
