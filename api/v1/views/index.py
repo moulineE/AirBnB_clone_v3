2bb2946a9aeb89ba3468d86e63ea475101aab0a7
moulineE <elmahdi.mouline@live.fr>
2023-10-30 04:51:11 +0100

    Task 3. Status of your API FIX: testing for req2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,11 @@
 """the index bluprint"""
 
 from api.v1.views import app_views
-
+from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def returns_a_JSON():
     """
     returns a JSON: "status": "OK"
     """
-    return dict({"status": "OK"})
+    return jsonify({"status": "OK"})
