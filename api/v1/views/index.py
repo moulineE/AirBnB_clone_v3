547f632de0b69f9494f5e32165b97a4f4e4be8aa
moulineE <elmahdi.mouline@live.fr>
2023-10-30 18:49:01 +0100

    Task 3 using jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """the index bluprint"""
 
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -9,4 +10,4 @@ def returns_a_JSON():
     """
     returns a JSON: "status": "OK"
     """
-    return {'status': 'OK'}
+    return jsonify({"status": "OK"})
