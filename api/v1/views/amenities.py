63fbdcca606948f9955d3a059899d796ef343a15
moulineE <elmahdi.mouline@live.fr>
2023-10-31 03:13:28 +0100

    Task 8. Amenity FIX: typo2

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'])
-def get_amenities_list(state_id):
+def get_amenities_list():
     """get amenities"""
     amenities_list = []
     amenities = storage.all(Amenity)
@@ -24,7 +24,7 @@ def get_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         return abort(404)
-    return jsonify(city.to_dict())
+    return jsonify(amenity.to_dict())
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
